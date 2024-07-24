005131dfbd49a9705cc298f09127f1a8a8a5a9f7
Kauê Matheus <kauematheus7@hotmail.com>
2024-07-24 09:05:21 -0300

    Variáveis, funções
    Colisão com as paredes superior e inferior, Colisão com as raquetes, Marcar ponto e resetar bola

--- js/script.js
+++ js/script.js
@@ -20,3 +20,63 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+
+const ball = document.getElementById('ball');
+const leftPaddle = document.getElementById('left-paddle');
+const rightPaddle = document.getElementById('right-paddle');
+const leftScoreElement = document.getElementById('left-score');
+const rightScoreElement = document.getElementById('right-score');
+let leftScore = 0;
+let rightScore = 0;
+
+const gameBoard = document.getElementById('game-board');
+const boardReact = gameBoard.getBoundingClientRect();
+
+
+let ballX = 0;
+let ballY = 0;
+let ballDX = 2; // Velocidade da bola no eixo X
+let ballDY = 2; // Velocidade da bola no eixo Y
+let paddleSpeed = 10; // Velocidade das raquetes
+
+
+function moveBall() {
+    ballX += ballDX
+    ballY += ballY;
+
+
+    // Colisão com as paredes superior e inferior
+    if (ballY <= 0 || ballY + ball.clientHeight >= boardReact.height) {
+        ballDY = -ballDY;
+    }
+
+    // Colisão com as raquetes
+    if (ballX <= leftPaddle.offsetLeft + leftPaddle.clientWidth && 
+        ballY + ball.clientHeight >= leftPaddle.offsetTop &&
+        ballY <= leftPaddle.offsetTop + leftPaddle.clientHeight) {
+        ballDX = -ballDX;
+
+    }
+
+    if (ballX + ballX.clientWidth >= rightPaddle.offsetLeft &&
+        ballY + ball.clientHeight >= rightPaddle.offsetTop &&
+        ballY <= rightPaddle.offsetTop + rightPaddle.clientHeight) {
+        ballDX = -ballDX
+    }
+
+    // Marcar ponto e resetar bola
+    if (ballDX <= 0) {
+        rightScore++;
+        updateScore();
+        resetBall();
+    }
+
+    if (ballX + ball.clientWidth >= boardReact.width) {
+        leftScore++;
+        updateScore();
+        resetBall();
+
+    }
+    
+}
